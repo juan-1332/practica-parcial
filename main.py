fb4b7f60df5e13e8b974a061f88d799f111c6c66
Andres_Felipe <andresfelip410@gmail.com>
2026-09-21 17:45:45 -0500

    agregar opciones de consulta con filtros e implementacion de edicion de sucursales

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-
-from sucursales import crear_sucursal
+# main.py
+from sucursales import crear_sucursal, listar_sucursales, actualizar_sucursal
 
 def mostrar_menu():
     print("\n=================================")
     print("  GESTIÓN DE SUCURSALES (CRUD)   ")
     print("=================================")
     print("1. Registrar nueva sucursal")
+    print("2. Consultar / Filtrar sucursales")
+    print("3. Editar sucursal existente")
     print("0. Salir")
     print("=================================")
 
 def main():
-    # Lista donde se almacenarán temporalmente las sucursales
     sucursales = []
 
     while True:
@@ -19,6 +20,10 @@ def main():
 
         if opcion == "1":
             crear_sucursal(sucursales)
+        elif opcion == "2":
+            listar_sucursales(sucursales)
+        elif opcion == "3":
+            actualizar_sucursal(sucursales)
         elif opcion == "0":
             print("Saliendo del programa...")
             break
