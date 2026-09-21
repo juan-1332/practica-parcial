60bd403d56d5552f014564df33054467b15828f8
juan-1332 <juancastrom55@gamil.com>
2026-09-21 17:35:29 -0500

    implementacion del main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+
+from sucursales import crear_sucursal
+
+def mostrar_menu():
+    print("\n=================================")
+    print("  GESTIÓN DE SUCURSALES (CRUD)   ")
+    print("=================================")
+    print("1. Registrar nueva sucursal")
+    print("0. Salir")
+    print("=================================")
+
+def main():
+    # Lista donde se almacenarán temporalmente las sucursales
+    sucursales = []
+
+    while True:
+        mostrar_menu()
+        opcion = input("Seleccione una opción: ").strip()
+
+        if opcion == "1":
+            crear_sucursal(sucursales)
+        elif opcion == "0":
+            print("Saliendo del programa...")
+            break
+        else:
+            print(" Opción no válida. Intente de nuevo.")
+
+if __name__ == "__main__":
+    main()
